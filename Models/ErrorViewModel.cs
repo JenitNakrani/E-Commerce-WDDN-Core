c4cf0ba60b4d144d360d941b343c1c2d23c1325f
visrut navadiya <visrutnavadiya@gmail.com>
2021-09-20 04:00:51 +0530

    project initial setup
    project template with `dotnet new mvc` command individual authentication added.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace E_Commerce_WDDN_Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # E-Commerce-WDDN-Core
+
+- start project : `dotnet new mvc --auth Individual`
+- add gitignore : `dotnet new gitignore`
