7eb50ce9f8b307c7d2e7eeb8b0277ab840053833
visrut navadiya <visrutnavadiya@gmail.com>
2021-09-20 15:32:29 +0530

    database setup done

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using E_Commerce_WDDN_Core.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(E_Commerce_WDDN_Core.Areas.Identity.IdentityHostingStartup))]
+namespace E_Commerce_WDDN_Core.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
